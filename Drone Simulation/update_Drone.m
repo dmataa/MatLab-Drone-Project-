8644a35b26aeb2f6f683f6bc9b91daf7c1b8736d
colintracy <ctra4892@giant.cos.edu>
2026-04-20 02:48:07 -0700

    Update update_Drone.m

--- Drone Simulation/update_Drone.m
+++ Drone Simulation/update_Drone.m
@@ -3,15 +3,15 @@ if isempty(drone.target) || fire.intensity(drone.target(1), drone.target(2)) < 0
     drone.target = assign_Target(fire);
 end
 proposedPos = move_Drone(drone.pos, drone.target);
-[safePos, near_miss] = detect_Avoidance(proposedPos,drones,index, params.gridSize);
+[safePos, near_miss] = detect_Avoidance(proposedPos, drones, index, params);
 drone.distance = drone.distance + norm(safePos - drone.pos);
 drone.pos = safePos;
 drone.near_miss = drone.near_miss + near_miss;
 
-if isempty(drone.target)
+if ~isempty(drone.target)
     if isequal(drone.pos, drone.target)
         fire.intensity(drone.pos(1), drone.pos(2)) = 0;
-        drone.cells_extinguished = drone.cells_extinguished + 1
+        drone.cells_extinguished = drone.cells_extinguished + 1;
     end
 end
 drone.log(end + 1, :) = [drone.pos, drone.target];
