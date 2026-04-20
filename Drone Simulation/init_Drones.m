fb1711e687a75fd247323d470189a23e17822b32
colintracy <ctra4892@giant.cos.edu>
2026-04-20 02:46:40 -0700

    Update init_Drones.m

--- Drone Simulation/init_Drones.m
+++ Drone Simulation/init_Drones.m
@@ -1,7 +1,10 @@
 function drones = init_Drones(params)
-    for i = 1:params.numDrones
-        drones(i).pos = [randi(params.gridSize(1)), randi(params.gridSize(2))];
-        drones(i).target = [];
-        drones(i).log = [];
+    for ii = 1:params.numDrones
+        drones(ii).pos = [randi(params.rows), randi(params.cols)];
+        drones(ii).target = [];
+        drones(ii).log = [];
+        drones(ii).distance = 0;
+        drones(ii).near_miss = 0;
+        drones(ii).cells_extinguished = 0;
     end
 end
