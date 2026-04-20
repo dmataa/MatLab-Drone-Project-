12a6e0021bc9cc3b27f99278a3d2b907c8f973eb
colintracy <ctra4892@giant.cos.edu>
2026-04-20 14:08:38 -0700

    Update update_Drone.m

--- Drone Simulation/update_Drone.m
+++ Drone Simulation/update_Drone.m
@@ -1,18 +1,24 @@
-function[drone, fire] = update_Drone(drone, fire, params, drones, index)
-if isempty(drone.target) || fire.intensity(drone.target(1), drone.target(2)) < 0.1
-    drone.target = assign_Target(fire);
-end
-proposedPos = move_Drone(drone.pos, drone.target);
-[safePos, near_miss] = detect_Avoidance(proposedPos, drones, index, params);
-drone.distance = drone.distance + norm(safePos - drone.pos);
-drone.pos = safePos;
-drone.near_miss = drone.near_miss + near_miss;
-if ~isempty(drone.target)
+function [drone, fire] = update_Drone(drone, fire, params, drones, index)
+
+    if isempty(drone.target)
+        drone.target = assign_Target(fire);
+    end
+
+    if isempty(drone.target)
+        drone.log(end+1,:) = [drone.pos, NaN, NaN];
+        return;
+    end
+
+    proposedPos = move_Drone(drone.pos, drone.target);
+    [safePos, near_miss] = detect_Avoidance(proposedPos, drones, index, params);
+
+    drone.distance = drone.distance + norm(safePos - drone.pos);
+    drone.pos = safePos;
+    drone.near_miss = drone.near_miss + near_miss;
     if all(drone.pos == drone.target)
         fire.intensity(drone.pos(1), drone.pos(2)) = 0;
         drone.cells_extinguished = drone.cells_extinguished + 1;
         drone.target = [];
     end
-end
-drone.log(end + 1, :) = [drone.pos, drone.target];
-end
+
+    drone.log(end+1,:) = [drone.pos, drone.target];
