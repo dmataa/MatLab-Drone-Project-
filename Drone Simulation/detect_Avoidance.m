08a0468b275ad1e09be3bc9ccb3911f671b3709d
colintracy <ctra4892@giant.cos.edu>
2026-04-20 02:46:24 -0700

    Update detect_Avoidance.m

--- Drone Simulation/detect_Avoidance.m
+++ Drone Simulation/detect_Avoidance.m
@@ -1,9 +1,9 @@
-function [pos, near_miss] = detect_Avoidance(proposedPos, drones, index, gridSize)
+function [pos, near_miss] = detect_Avoidance(proposedPos, drones, index, params)
 near_miss = 0;
 pos = proposedPos;
 
-pos(1) = max(1, min(gridSize(1), pos(1)));
-pos(2) = max(1, min(gridSize(2), pos(1)));
+pos(1) = max(1, min(params.rows, pos(1)));
+pos(2) = max(1, min(params.cols, pos(2)));
 
 for ii = 1:length(drones)
     if ii ~= index
